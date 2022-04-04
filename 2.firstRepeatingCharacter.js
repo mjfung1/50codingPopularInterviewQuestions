024ebb2839c1736424a638049561ec0dfc9c2f9c
Miguel Fung <mjfung1@gmail.com>
2022-04-04 19:37:52 -0400

    use visited. similar to find pair sum

---
+++ 2.firstRepeatingCharacter.js
@@ -0,0 +1,16 @@
+
+// time: O(n)
+// space: O(n)
+
+function firstRepeatingCharacter(str){
+  // your code here
+  let visited = new Set();
+  
+  for (let char of str) {
+      if (visited.has(char)) return char;
+      
+      visited.add(char);
+  }
+  
+  return '\0'
+}
